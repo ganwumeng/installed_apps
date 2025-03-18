254eb6e94a4a9032eab91c3b43d547441e52ed52
Ganwumeng <38616251+ganwumeng@users.noreply.github.com>
2025-03-18 20:33:55 +0800

    Remove getPlatform()

--- android/src/main/kotlin/com/sharmadhiraj/installed_apps/BuiltWithUtil.kt
+++ android/src/main/kotlin/com/sharmadhiraj/installed_apps/BuiltWithUtil.kt
@@ -10,20 +10,7 @@ class BuiltWithUtil {
     companion object {
 
         fun getPlatform(applicationInfo: ApplicationInfo): String {
-            val apkPath = applicationInfo.sourceDir
-            val zipFile = ZipFile(apkPath)
-            val entries: List<String> = zipFile.entries().toList().map { entry -> entry.name }
-            return if (isFlutterApp(entries)) {
-                "flutter"
-            } else if (isReactNativeApp(entries)) {
-                "react_native"
-            } else if (isXamarinApp(entries)) {
-                "xamarin"
-            } else if (isIonicApp(entries)) {
-                "ionic"
-            } else {
-                "native_or_others"
-            }
+            return "native_or_others"
         }
 
         private fun isFlutterApp(entries: List<String>): Boolean {
